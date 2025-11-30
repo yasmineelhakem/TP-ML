9ec1d8dd11151de80bd55a42f248b6e4bcb9959f
yasmine <yasmine.elhakem@insat.ucar.tn>
2025-11-30 23:22:50 +0100

    tp1

--- tp1/q5.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-y = np.array([35, 25, 25, 15])
-mylabels = ["MPI", "RT", "IMI", "IIA"]
-plt.pie(y, labels = mylabels)
-plt.legend(title = "Four classes:", loc=2)
-plt.show()
